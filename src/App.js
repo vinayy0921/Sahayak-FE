19d2f8757d84689b3afa928df97174ebb3e9bdf3
shivam <shivamsingo96@gmail.com>
2025-12-01 14:56:27 +0530

    login & register page add

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import LandingPage from "./pages/LandingPage";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
 
-        </Routes>
-      </Router>
-
-    </>
+        {/* Login & Signup same page, animation ke saath */}
+        <Route path="/login" element={<AuthPage initialMode="login" />} />
+        <Route path="/signup" element={<AuthPage initialMode="signup" />} />
+      </Routes>
+    </Router>
   );
 }
 
